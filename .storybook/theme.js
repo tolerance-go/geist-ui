685174fd011e77757d72828e934c4c35ce012a49
tolerance-go <892728595@qq.com>
2022-11-12 01:16:28 +0800

    change brandTitle

--- .storybook/theme.js
+++ .storybook/theme.js
@@ -2,7 +2,7 @@ import { create } from '@storybook/theming'
 
 export default create({
     base: 'light',
-    brandTitle: '@fenxing/mandong-core',
+    brandTitle: '@fenxing/mandong-ui',
     colorPrimary: '#000',
     colorSecondary: '#000',
 })
